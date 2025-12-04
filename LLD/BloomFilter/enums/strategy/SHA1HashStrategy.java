91ea891ff1245d82b5aa397c1e81d2a9c344055c
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2025-12-04 19:26:05 +0530

    Create SHA1HashStrategy.java

---
+++ LLD/BloomFilter/enums/strategy/SHA1HashStrategy.java
@@ -0,0 +1,14 @@
+package LLD_Interview_Problems.BloomFilter.strategy;
+
+public class SHA1HashStrategy implements HashStrategy {
+
+    @Override
+    public int calculateHash(String data){
+        int hash = 7;
+        for(int i = 0; i < data.length(); i++){
+            hash = (hash * 31 + data.charAt(i)) % Integer.MAX_VALUE;
+        }
+        return hash;
+
+    }
+}
