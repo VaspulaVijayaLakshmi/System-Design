9228a6a686cfbff71c82b325e609ff951ecb6bfa
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2025-12-04 19:32:22 +0530

    Create IllegalMoveException.java

---
+++ LLD/SnakeAndLadder/entities/enums/exceptons/IllegalMoveException.java
@@ -0,0 +1,8 @@
+package LLD_Interview_Problems.SnakeAndLadder.exceptions;
+
+public class IllegalMoveException extends RuntimeException{
+
+    public IllegalMoveException(String message){
+        super(message);
+    }
+}
