c06615c1d6df507efb4827fbe7609c68839d67a4
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2025-12-04 19:32:38 +0530

    Create IllegalPositioningException.java

---
+++ LLD/SnakeAndLadder/entities/enums/exceptons/IllegalPositioningException.java
@@ -0,0 +1,8 @@
+package LLD_Interview_Problems.SnakeAndLadder.exceptions;
+
+public class IllegalPositioningException extends RuntimeException {
+    public IllegalPositioningException(String message) {
+        super(message);
+    }
+}
+
