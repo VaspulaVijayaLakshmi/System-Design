07200e3a53eb308d18b517368ac42f47389d8e56
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2025-12-04 19:05:03 +0530

    Create AunctionHouse.java

---
+++ Design Patterns/Behavioural/Mediator/AunctionHouse.java
@@ -0,0 +1,21 @@
+package Behavioural.Mediator;
+
+public interface AuctionHouse {
+
+
+     void registerBidder(String bidder);
+     void placeBid(String bidder, double amount);
+
+
+//    Multiple Auctions
+
+//    void registerAuctioner(Auctioner auctioneer);
+//
+//     void AssignAuctionerToAuction(Auctioner auctioner);
+//
+//     void openAuction();
+//     void closeAuction();
+//
+//     void announceWinner();
+
+}
