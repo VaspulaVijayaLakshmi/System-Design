ba8b557724aa92619c1838f42aa0122d76f26009
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2025-12-04 19:18:19 +0530

    Create IllegalMoveException.java

---
+++ LLD/TicTacToe/exceptions/IllegalMoveException.java
@@ -0,0 +1,10 @@
+package LLD_Interview_Problems.TicTacToe.exceptions;
+
+public class IllegalMoveException extends RuntimeException {
+
+    public IllegalMoveException(String message){
+        super(message);
+    }
+
+
+}
