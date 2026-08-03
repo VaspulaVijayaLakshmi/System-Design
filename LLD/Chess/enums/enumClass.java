7940359ed4909bf89c7e3ef71a1fb58cf91c2d61
Vijaya <68328513+VaspulaVijayaLakshmi@users.noreply.github.com>
2026-08-04 01:15:04 +0530

    Create enumClass.java

---
+++ LLD/Chess/enums/enumClass.java
@@ -0,0 +1,18 @@
+enum Color {
+    WHITE,
+    BLACK
+}
+
+enum GameStatus {
+    STARTED,
+    FINISHED
+}
+
+enum PieceType {
+    KING,
+    QUEEN,
+    ROOK,
+    BISHOP,
+    KNIGHT,
+    PAWN
+}
